f4e18de85804d8815ced786f9ab4842d99ed42b9
Abhiraj Kadam <abhirajmohankadam@gmail.com>
2026-06-01 19:08:55 +0530

    Created practice_3 with Variables

---
+++ practice_3.java
@@ -0,0 +1,8 @@
+public class practice_3 {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        int c = 2 * (a + b);
+        System.out.println(c);
+    }
+}
