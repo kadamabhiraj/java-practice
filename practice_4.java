b1cefce8f81a928cd117bd1d70495da4a4d16758
Abhiraj Kadam <abhirajmohankadam@gmail.com>
2026-06-02 13:16:35 +0530

    Created practice_4 with data types

---
+++ practice_4.java
@@ -0,0 +1,18 @@
+public class practice_4 {
+    public static void main (String args[]){
+        byte a = 10;
+        System.out.println(a);
+        char b = 'C';
+        System.out.println(b);
+        boolean c = true;
+        System.out.println(c);
+        float d = 4.4F;
+        System.out.println(d);
+        int e = 10;
+        System.out.println(e);
+        long f = 100000000L;
+        System.out.println(f);
+        double g = 3.142D;
+        System.out.println(g);
+    }
+}
